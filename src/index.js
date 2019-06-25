612a1bb4b61145e177d4da717615fe753c701ee6
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-25 11:06:35 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const button = document.getElementById("create-new-task")
+  button.addEventListener('click', function(e){
+    e.preventDefault()
+    const text = document.getElementById("new-task-description");
+    const tasks = document.getElementById("tasks");
+    const newItem = document.createElement("li")
+    newItem.innerText = text.value
+    tasks.append(newItem)
+  })
 });
