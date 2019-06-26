f12e8d6f118fd124571c79a6bf2f68f407e6a662
Alex Johnston <alexjohnston@ymail.com>
2019-06-26 15:01:59 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasks = document.getElementById("tasks");
+
+  document.getElementById("create-task-form").addEventListener("submit", submitTask);
+});
+
+let i = 1
+
+submitTask = (e) => {
+  e.preventDefault();
+  let task = document.getElementById("task").value;
+  const ulTask = document.createElement("li");
+  ulTask.innerHTML = task + `<button id=${i}>Delete</button>`;
+  i++
+  tasks.append(ulTask);
+}
+
+//delete button function
+
+tasks.addEventListener("click", (e) => {
+  e.preventDefault();
+  button = document.getElementById(`${i-1}`);
+  button.parentNode.remove();
+  i--
 });
