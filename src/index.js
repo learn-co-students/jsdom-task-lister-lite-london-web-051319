94d719f030ca0abfe3a60ac50c6eb1c3616ea160
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-27 13:20:44 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.querySelector("form#create-task-form")
+
+  function newToDO(string){
+    let listEl = document.querySelector("#tasks")
+    let liEl = document.createElement("li")
+    liEl.innerHTML +=`<div class="task-element">${string}</div>`
+    let del = document.createElement("button");
+    del.innerHTML = "Delete"
+
+    listEl.append(liEl, del)
+  };
+
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    let inputEl = document.querySelector("input#new-task-description")
+    let text = inputEl.value
+    newToDO(text)
+    form.reset()
+  });
+
+  del.addEventListener('click', event =>{
+    event.preventDefault()
+    this.parentElement.remove();
+ });
+
 });
